f5ba09fffa5e010aa88b9e24d1d26760672dc694
Amanda Zanata <amandazanata46@gmail.com>
2023-04-20 18:40:55 -0300

    refatora código app.js

--- src/app.js
+++ src/app.js
@@ -1,61 +1,65 @@
 // IMPORTA E INICIA PACOTE EXPRESS
 const express = require('express');
 
-const teams = [
-    {
-    id: 1,
-    name: 'São Paulo Futebol Clube',
-    initials: 'SPF',
-    },
-    {
-    id: 2,
-    name: 'Clube Atlético Mineiro',
-    initials: 'CAM',
-    },
-];
-
 const app = express();
+
+const teams = [
+    { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+    { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
 
 app.use(express.json());
 
-// app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
-
 // Arquitetura REST
-app.get('/teams', (req, res) => res.status(200).json({ teams }));
+app.get('/teams', (_req, res) => res.json(teams));
 
-app.get('/teams/:id', (req, res) => { // Crie um endpoint do tipo GET com a rota /teams/:id
-    const team = teams.find(({ id }) => id === Number(req.params.id));
-    res.status(200).json(team);
+let nextId = 3;
+
+app.get('/teams/:id', (req, res) => { // Endpoint do tipo GET com a rota /teams/:id
+    const id = Number(req.params.id); // Recebe como string, então faz um parse Number para normalizar o número
+    const team = teams.find((t) => t.id === id);
+    if (team) {
+        res.json(team);
+    } else {
+        res.sendStatus(404); // sendStatus função nativa do express
+    }
 });
 
 app.post('/teams', (req, res) => {
-    const newTeam = { ...req.body };
-    teams.push(newTeam);
-    
-    res.status(201).json({ team: newTeam });
+    const requiredProperties = ['nome', 'sigla'];
+    if (requiredProperties.every((property) => property in req.body)) {
+        const team = { id: nextId, ...req.body };
+        teams.push(team);
+        nextId += 1;
+        res.status(201).json(team);
+    } else {
+        res.sendStatus(400);
+    }
 });
 
 app.put('/teams/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, initials } = req.body;
-
-    const updateTeam = teams.find((team) => team.id === Number(id));
-
-    if (!updateTeam) {
-    res.status(404).json({ message: 'Team not found' });
+    const id = Number(req.params.id);
+    const requiredProperties = ['nome', 'sigla'];
+    const team = teams.find((t) => t.id === id);
+    if (team && requiredProperties.every((property) => property in req.body)) {
+        const index = teams.indexOf(team);
+        const updated = { id, ...req.body };
+        teams.splice(index, 1, updated);
+        res.status(201).json(updated);
+    } else {
+        res.sendStatus(400);
     }
-
-    updateTeam.name = name;
-    updateTeam.initials = initials;
-    res.status(200).json({ updateTeam });
 });
 
 app.delete('/teams/:id', (req, res) => {
-    const { id } = req.params;
-    const arrayPosition = teams.findIndex((team) => team.id === Number(id));
-    teams.splice(arrayPosition, 1);
-
-    res.status(200).end();
+    const id = Number(req.params.id);
+    const team = teams.find((t) => t.id === id);
+    if (team) {
+        const index = teams.indexOf(team);
+        teams.splice(index, 1);
+    } else {
+        res.sendStatus(204);
+    }
 });
 
 module.exports = app;
